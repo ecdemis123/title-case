3f53d2b5aa6171a61f4013f8daac15cb8ef05e97
Eleanor Demis <eleanordemis@gmail.com>
2016-05-18 15:58:42 -0700

    adds tests for title cases

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -2,5 +2,12 @@ require('rspec')
 require('title_case')
 
 describe('String#title_case') do
-  #eventually put specs here
+  it("capitalizes the first letter of a word") do
+    expect(("beowulf").title_case()).to(eq("Beowulf"))
+  end
+
+  it("capitalizes the first letter of all words in a multiple word title") do
+    expect(("the color purple").title_case()).to(eq("The Color Purple"))
+  end
+
 end
