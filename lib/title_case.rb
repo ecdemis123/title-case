1376348113527caaf1bacfbc742accfaae618c97
Eleanor Demis <eleanordemis@gmail.com>
2016-05-18 16:30:29 -0700

    adds title form view

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -2,7 +2,6 @@ class String
   define_method(:title_case) do
     split_sentence = self.split()
     split_sentence.each() do |word|
-      binding.pry
       word.capitalize!()
     end
     split_sentence.join(" ")
