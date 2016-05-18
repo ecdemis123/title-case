7dfb2d25ea5f5d16d811c4e85a2a69e4fbe714f3
Eleanor Demis <eleanordemis@gmail.com>
2016-05-18 16:23:57 -0700

    adds pry

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,7 +1,8 @@
 class String
   define_method(:title_case) do
-    split_sentence = self.split
-    split_sentence.each do |word|
+    split_sentence = self.split()
+    split_sentence.each() do |word|
+      binding.pry
       word.capitalize!()
     end
     split_sentence.join(" ")
