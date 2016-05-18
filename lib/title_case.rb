04a24c377d47361cd2deb4fec6c680df8d5cb38c
Eleanor Demis <eleanordemis@gmail.com>
2016-05-18 16:55:20 -0700

    fixes logic for capitalized words

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,8 +1,11 @@
 class String
   define_method(:title_case) do
+    lowercase_words = ["and"]
     split_sentence = self.split()
     split_sentence.each() do |word|
-      word.capitalize!()
+      if !lowercase_words.include?(word)
+        word.capitalize!()
+      end
     end
     split_sentence.join(" ")
   end
